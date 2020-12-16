a69c7b504adc852babaa156c4450af96fadda284
craiglmccarthy <craiglmccarthy@gmail.com>
2020-12-16 15:34:37 +0000

    Initial commit

---
+++ get_links.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+A tool to get all links on a webpage from a given list of URLs.
+Including an option to exclude external links.
+"""
+
+import argparse
+
+import requests
+
+# TODO 1 Get a list of URLs
+# TODO 2 Give option of authenticating
+# TODO 3 Loop through list of URLs, visit one page at a time
+# TODO 4 Scrape <a> hrefs from page - add to list
+
+def main():
+    # TODO CLI arguments
+    pass
+
+
+if __name__ == '__main__':
+    main()
