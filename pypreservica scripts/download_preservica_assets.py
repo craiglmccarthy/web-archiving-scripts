8ebd2e5e0f9444edc6373099995eceb647a23805
Craig McCarthy <craiglmccarthy@gmail.com>
2022-12-08 16:08:22 +0000

    Changed quotation marks

--- pypreservica scripts/download_preservica_assets.py
+++ pypreservica scripts/download_preservica_assets.py
@@ -20,7 +20,7 @@ PASSWORD = os.getenv('PASSWORD')
 TENANT = os.getenv('TENANT')
 SERVER = os.getenv('SERVER')
 
-FOLDER_REF = "5b2391ee-b6cb-4091-8339-83d1f56fa38b"
+FOLDER_REF = '5b2391ee-b6cb-4091-8339-83d1f56fa38b'
 
 client = EntityAPI(username=USERNAME,
                    password=PASSWORD, tenant=TENANT, server=SERVER)
@@ -29,7 +29,7 @@ folder = client.folder(FOLDER_REF)
 
 # Get child assets individually and download them
 for asset in filter(only_assets, client.all_descendants(folder.reference)):
-    print(f"Downloading {asset.title}...")
+    print(f'Downloading {asset.title}...')
     asset = client.asset(asset.reference)
     # Preservica strips the .gz extension from asset.title, this adds it back on
     client.download(asset, asset.title + '.gz')
