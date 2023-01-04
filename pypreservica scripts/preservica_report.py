c51da2526645a366d97eb38650a2b8b95bdfbbc5
Craig McCarthy <craiglmccarthy@gmail.com>
2023-01-04 14:26:31 +0000

    Change var name from 'e' to 'entity'

--- pypreservica scripts/preservica_report.py
+++ pypreservica scripts/preservica_report.py
@@ -38,45 +38,53 @@ with open(CSV_OUTPUT_FILENAME, 'w', encoding='UTF8', newline='') as f:
 
     # Leave argument empty for root folder
     # Filter can be applied via: for asset in filter(only_assets, client.all_descendants()):
-    for e in client.all_descendants('83d5a7d0-47a5-46ce-b1f7-21f9a6a2ff0c'):
+    for entity in client.all_descendants():
 
-        print(e)  # Print folder/asset details
+        # print(e)  # Print folder/asset details
 
-        if e.parent != None:  # Gets around calling e.parent etc. on root implied root reference in all_descendants()
+        if entity.parent != None:  # Gets around calling entity.parent etc. on root implied root reference in all_descendants()
             filepath_string = []
-            folder = client.folder(e.parent)
+            folder = client.folder(entity.parent)
             filepath_string.insert(0, folder.title + '/')
             while folder.reference not in root_folder_references:  # Get parent folders recursively to the root
                 folder = client.folder(folder.parent)
                 filepath_string.insert(0, folder.title + '/')
 
             # Comment out to remove folders from output
-            if str(e.entity_type) == 'EntityType.FOLDER':
+            if str(entity.entity_type) == 'EntityType.FOLDER':
                 # Get Title from metadata
-                folder = client.folder(e.reference)
+                folder = client.folder(entity.reference)
                 for url, schema in folder.metadata.items():
                     # Search the http://www.openarchives.org/OAI/2.0/oai_dc/ schema only
                     if schema == 'http://www.openarchives.org/OAI/2.0/oai_dc/':
                         root = ET.fromstring(client.metadata(url))
-                        metadata_title = root[0].text  # Title field
+                        try:
+                            metadata_title = root[0].text  # Title field
+                        except Exception as e:
+                            metadata_title = ''
+                            print(e, folder.title)
                     else:
                         metadata_title = ''
                 writer.writerow(
                     [''.join(filepath_string) + folder.title, folder.reference, '', '', '', folder.security_tag, metadata_title])
 
             # Comment out to remove assets from output
-            if str(e.entity_type) == 'EntityType.ASSET':
+            if str(entity.entity_type) == 'EntityType.ASSET':
                 # Get Title from metadata
-                asset = client.asset(e.reference)
+                asset = client.asset(entity.reference)
                 for url, schema in asset.metadata.items():
                     # Search the http://www.openarchives.org/OAI/2.0/oai_dc/ schema only
                     if schema == 'http://www.openarchives.org/OAI/2.0/oai_dc/':
                         root = ET.fromstring(client.metadata(url))
-                        metadata_title = root[0].text  # Title field
+                        try:
+                            metadata_title = root[0].text  # Title field
+                        except Exception as e:
+                            metadata_title = ''
+                            print(e, asset.title)
                     else:
                         metadata_title = ''
 
-                for representation in client.representations(e):
+                for representation in client.representations(entity):
                     # print(representation.rep_type)
                     # print(representation.name)
                     # print(representation.asset.title)
@@ -89,17 +97,21 @@ with open(CSV_OUTPUT_FILENAME, 'w', encoding='UTF8', newline='') as f:
                         # print(content_object.metadata)
                         # print(content_object.asset.title)
 
-                        for generation in client.generations(content_object):
-                            # print('GENERATION:')
-                            # print(generation.original)
-                            # print(generation.active)
-                            # print(generation.content_object)
-                            # print(generation.format_group)
-                            # print(generation.effective_date)
-                            # print(generation.bitstreams)
-                            # print(generation.bitstreams[0].length, 'bytes')
-                            # print('FILEPATH:', ''.join(filepath_string) + generation.bitstreams[0].filename)
-                            # print('SIZE:', generation.bitstreams[0].length)
-                            # print('DATE:', generation.effective_date)
-                            writer.writerow([''.join(filepath_string) + generation.bitstreams[0].filename, e.reference, generation.content_object.reference, str(
-                                generation.bitstreams[0].length), generation.effective_date, generation.content_object.security_tag, metadata_title])
+                        try:
+                            for generation in client.generations(content_object):
+                                # print('GENERATION:')
+                                # print(generation.original)
+                                # print(generation.active)
+                                # print(generation.content_object)
+                                # print(generation.format_group)
+                                # print(generation.effective_date)
+                                # print(generation.bitstreams)
+                                # print(generation.bitstreams[0].length, 'bytes')
+                                # print('FILEPATH:', ''.join(filepath_string) + generation.bitstreams[0].filename)
+                                # print('SIZE:', generation.bitstreams[0].length)
+                                # print('DATE:', generation.effective_date)
+                                writer.writerow([''.join(filepath_string) + generation.bitstreams[0].filename, entity.reference, generation.content_object.reference, str(
+                                    generation.bitstreams[0].length), generation.effective_date, generation.content_object.security_tag, metadata_title])
+                        except Exception as e:
+                            print(e)
+                            writer.writerow([asset.title, 'ERROR', 'ERROR', 'ERROR', 'ERROR', 'ERROR', 'ERROR'])
